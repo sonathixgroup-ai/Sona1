2495e608c6bd54ff88ca3fd1a31a360a6a1a5c00
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-02 07:45:41 +0300

    Update app_router.dart

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1087,7 +1087,7 @@ class AppRouter {
         GoRoute(
           path: '/sante/doctor/messages/new',
           name: 'doctorChatNew',
-          pageBuilder: (context, state) => NoTransitionPage(child: DoctorChatPage(chatId: Id)), // chatId
+          pageBuilder: (context, state) => NoTransitionPage(child: DoctorChatPage(chatId: '')), // chatId
         ),
         GoRoute(
           path: '/sante/doctor/alerts',
