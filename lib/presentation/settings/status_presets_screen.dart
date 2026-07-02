6be444085cdb23981d7d37be2e411284f27faf15
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-02 18:39:02 +0300

    Add StatusPresetsScreen widget for status presets

---
+++ lib/presentation/settings/status_presets_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:thix_id/presentation/chat/online_status/status_presets.dart';
+
+class StatusPresetsScreen extends StatelessWidget {
+  const StatusPresetsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const StatusPresets();
+  }
+}
