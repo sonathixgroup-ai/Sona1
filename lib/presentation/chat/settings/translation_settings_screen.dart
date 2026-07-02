51041dda6bc5b14a82d443c9bda127bc8724b72c
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-02 19:56:04 +0300

    Add TranslationSettingsScreen widget

---
+++ lib/presentation/chat/settings/translation_settings_screen.dart
@@ -0,0 +1,12 @@
+// presentation/chat/settings/translation_settings_screen.dart
+import 'package:flutter/material.dart';
+import '../translation/auto_translate_settings.dart';
+
+class TranslationSettingsScreen extends StatelessWidget {
+  const TranslationSettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AutoTranslateSettings();
+  }
+}
