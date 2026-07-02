def0fc2bdc43d5b12a6acfdc8d1488096765a766
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-02 19:53:20 +0300

    Add ChatStatusUpdateScreen widget

---
+++ lib/presentation/chat/screens/chat_status_update_screen.dart
@@ -0,0 +1,12 @@
+// presentation/chat/screens/chat_status_update_screen.dart
+import 'package:flutter/material.dart';
+import '../chat_status_update.dart';
+
+class ChatStatusUpdateScreen extends StatelessWidget {
+  const ChatStatusUpdateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ChatStatusUpdatePage();
+  }
+}
