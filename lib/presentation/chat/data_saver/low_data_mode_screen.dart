99a3479c872c28820cade04f2483a6c3d798ab69
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-02 18:50:45 +0300

    Add LowDataModeScreen widget for low data mode

---
+++ lib/presentation/chat/data_saver/low_data_mode_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:thix_id/presentation/chat/data_saver/low_data_mode_toggle.dart';
+
+class LowDataModeScreen extends StatelessWidget {
+  const LowDataModeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LowDataModeToggle();
+  }
+}
