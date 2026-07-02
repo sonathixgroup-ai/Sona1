164629ebc39bb8bf8228d08fad658932230e3939
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-02 18:51:56 +0300

    Add WidgetPreviewScreen to display widget previews

---
+++ lib/presentation/chat/home_widgets/widget_preview_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:thix_id/presentation/chat/home_widgets/widget_preview.dart';
+
+class WidgetPreviewScreen extends StatelessWidget {
+  const WidgetPreviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const WidgetPreview();
+  }
+}
