e87ed04cf954b91474b9df94b447b7c13fa6e67d
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-02 19:56:30 +0300

    Add ThemeSelectorScreen for theme selection

---
+++ lib/presentation/chat/settings/theme_selector_screen.dart
@@ -0,0 +1,12 @@
+// presentation/chat/settings/theme_selector_screen.dart
+import 'package:flutter/material.dart';
+import '../themes/theme_selector_sheet.dart';
+
+class ThemeSelectorScreen extends StatelessWidget {
+  const ThemeSelectorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ThemeSelectorSheet();
+  }
+}
