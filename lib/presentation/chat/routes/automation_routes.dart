b495dd4f28ed345e8691ec95586459b6f46af3df
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-02 14:53:14 +0300

    Delete lib/presentation/chat directory

--- lib/presentation/chat/routes/automation_routes.dart
+++
@@ -1,9 +0,0 @@
-/// Automation & Scheduling routes
-class AutomationRoutes {
-  static const String scheduledMessages = '/automation/scheduled-messages';
-  static const String autoReplies = '/automation/auto-replies';
-  static const String messageTemplates = '/automation/message-templates';
-  static const String workflowRules = '/automation/workflow-rules';
-  static const String reminders = '/automation/reminders';
-  static const String botCommands = '/automation/bot-commands';
-}
