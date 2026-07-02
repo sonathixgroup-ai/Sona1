5c9853cc978bdb33afb151d848f2adc54685d74a
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-02 18:38:26 +0300

    Add AvailabilityScheduleScreen widget

---
+++ lib/presentation/settings/availability_schedule_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:thix_id/presentation/chat/online_status/availability_schedule.dart';
+
+class AvailabilityScheduleScreen extends StatelessWidget {
+  const AvailabilityScheduleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AvailabilitySchedule();
+  }
+}
