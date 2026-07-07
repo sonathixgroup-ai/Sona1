ed5b81959e5686fe4657f6b69bd73663094d25c1
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-07 21:45:57 +0300

    Create course_list_page.dart

---
+++ lib/presentation/education/instructor/courses/course_list_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CourseListPage extends StatelessWidget {
+  const CourseListPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mes cours')),
+      body: const Center(child: Text('Liste des cours (à implémenter)')),
+    );
+  }
+}
