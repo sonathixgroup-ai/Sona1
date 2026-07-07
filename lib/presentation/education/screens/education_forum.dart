500ffdfda7e53a8fc959094e9a035b32a927bce6
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-07 15:45:49 +0300

    Refactor EducationForum to streamline topic loading

--- lib/presentation/education/screens/education_forum.dart
+++ lib/presentation/education/screens/education_forum.dart
@@ -2,15 +2,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
-import '../../../providers/forum_provider.dart';
-import '../../../providers/education_provider.dart';
-import '../widgets/common/education_empty_state.dart';
-import '../widgets/forum/forum_topic_card.dart';
-import '../widgets/forum/forum_create_topic_dialog.dart';
 import 'package:thix_id/presentation/education/providers/forum_provider.dart';
+import 'package:thix_id/presentation/education/models/forum_topic.dart';
+import 'package:thix_id/presentation/education/widgets/forum/forum_topic_card.dart';
+import 'package:thix_id/presentation/education/widgets/forum/forum_create_topic_dialog.dart';
+import 'package:thix_id/presentation/education/widgets/common/education_empty_state.dart';
+
 class EducationForum extends StatefulWidget {
   final String formationId;
-
   const EducationForum({super.key, required this.formationId});
 
   @override
@@ -22,13 +21,8 @@ class _EducationForumState extends State<EducationForum> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _loadData();
+      context.read<ForumProvider>().loadTopics(widget.formationId);
     });
-  }
-
-  Future<void> _loadData() async {
-    final provider = context.read<ForumProvider>();
-    await provider.loadTopics(widget.formationId);
   }
 
   @override
@@ -39,22 +33,24 @@ class _EducationForumState extends State<EducationForum> {
     return Scaffold(
       backgroundColor: const Color(0xFFF7FAFF),
       appBar: AppBar(
-        title: const Text(
-          'Forum de la formation',
-          style: TextStyle(fontWeight: FontWeight.w800, color: Color(0xFF1A1A2E)),
-        ),
+        title: const Text('Forum', style: TextStyle(fontWeight: FontWeight.W800, color: Color(0xFF1A1A2E))),
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_rounded, color: Color(0xFF1A1A2E)),
-          onPressed: () => Navigator.pop(context),
-        ),
         actions: [
           IconButton(
             icon: const Icon(Icons.add_rounded, color: Color(0xFF1A1A2E)),
-            onPressed: _showCreateTopicDialog,
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (_) => ForumCreateTopicDialog(formationId: widget.formationId),
+              ).then((_) => provider.loadTopics(widget.formationId));
+            },
           ),
         ],
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_rounded, color: Color(0xFF1A1A2E)),
+          onPressed: () => context.pop(),
+        ),
       ),
       body: provider.isLoading
           ? const Center(child: CircularProgressIndicator())
@@ -64,7 +60,12 @@ class _EducationForumState extends State<EducationForum> {
                   subtitle: 'Soyez le premier à poser une question',
                   icon: Icons.forum_rounded,
                   buttonText: 'Créer un sujet',
-                  onButtonPressed: _showCreateTopicDialog,
+                  onButtonPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (_) => ForumCreateTopicDialog(formationId: widget.formationId),
+                    ).then((_) => provider.loadTopics(widget.formationId));
+                  },
                 )
               : ListView.builder(
                   padding: const EdgeInsets.all(16),
@@ -75,23 +76,11 @@ class _EducationForumState extends State<EducationForum> {
                       padding: const EdgeInsets.only(bottom: 12),
                       child: ForumTopicCard(
                         topic: topic,
-                        onTap: () => context.push(
-                          '/education/forum/topic/${topic.id}',
-                          extra: topic,
-                        ),
+                        onTap: () => context.push('/education/forum/topic/${topic.id}'),
                       ),
                     );
                   },
                 ),
     );
-  }
-
-  void _showCreateTopicDialog() {
-    showDialog(
-      context: context,
-      builder: (context) => ForumCreateTopicDialog(
-        formationId: widget.formationId,
-      ),
-    ).then((_) => _loadData());
   }
 }
