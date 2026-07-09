be3fbe0b58307e2ea4866942fe1ff0cf4aad0c84
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-09 23:10:45 +0300

    Add documentaries provider for Riverpod integration

---
+++ lib/presentation/mon_pays/providers/providers/documentaries_provider.dart
@@ -0,0 +1,16 @@
+// lib/presentation/mon_pays/providers/documentaries_provider.dart
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/documentary_model.dart';
+import '../repositories/documentaries_repository.dart';
+import 'mon_pays_provider.dart';
+
+final documentariesProvider = FutureProvider<List<Documentary>>((ref) async {
+  final repo = ref.watch(documentariesRepositoryProvider);
+  return repo.getAll();
+});
+
+final documentaryProvider = FutureProvider.family<Documentary, String>((ref, id) async {
+  final repo = ref.watch(documentariesRepositoryProvider);
+  return repo.getById(id);
+});
