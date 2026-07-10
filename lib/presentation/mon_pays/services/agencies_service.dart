efa08f7e8421d1a903453a4817ae4770bca968c0
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-10 14:46:06 +0300

    Update agencies_service.dart

--- lib/presentation/mon_pays/services/agencies_service.dart
+++ lib/presentation/mon_pays/services/agencies_service.dart
@@ -1,41 +1,52 @@
 // lib/presentation/mon_pays/services/agencies_service.dart
 
-import 'package:dio/dio.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../models/agency_model.dart';
-import '../utils/mon_pays_constants.dart';
 
 class AgenciesService {
-  final Dio _dio;
+  final SupabaseClient _supabase;
 
-  AgenciesService(this._dio);
+  AgenciesService(this._supabase);
 
   Future<List<Agency>> getAll() async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.agenciesEndpoint}');
-    return (response.data as List).map((e) => Agency.fromJson(e)).toList();
+    final response = await _supabase
+        .from('agencies')
+        .select('*');
+    return (response as List).map((e) => Agency.fromJson(e)).toList();
   }
 
   Future<Agency> getById(String id) async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.agenciesEndpoint}/$id');
-    return Agency.fromJson(response.data);
+    final response = await _supabase
+        .from('agencies')
+        .select('*')
+        .eq('id', id)
+        .single();
+    return Agency.fromJson(response);
   }
 
   Future<Agency> create(Agency agency) async {
-    final response = await _dio.post(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.agenciesEndpoint}',
-      data: agency.toJson(),
-    );
-    return Agency.fromJson(response.data);
+    final response = await _supabase
+        .from('agencies')
+        .insert(agency.toJson())
+        .select()
+        .single();
+    return Agency.fromJson(response);
   }
 
   Future<Agency> update(Agency agency) async {
-    final response = await _dio.put(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.agenciesEndpoint}/${agency.id}',
-      data: agency.toJson(),
-    );
-    return Agency.fromJson(response.data);
+    final response = await _supabase
+        .from('agencies')
+        .update(agency.toJson())
+        .eq('id', agency.id)
+        .select()
+        .single();
+    return Agency.fromJson(response);
   }
 
   Future<void> delete(String id) async {
-    await _dio.delete('${MonPaysConstants.baseUrl}${MonPaysConstants.agenciesEndpoint}/$id');
+    await _supabase
+        .from('agencies')
+        .delete()
+        .eq('id', id);
   }
 }
