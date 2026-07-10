da82b8cc0142cb8b42b3b5f9986e768306d84409
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-10 14:45:00 +0300

    Update government_service.dart

--- lib/presentation/mon_pays/services/government_service.dart
+++ lib/presentation/mon_pays/services/government_service.dart
@@ -1,41 +1,52 @@
 // lib/presentation/mon_pays/services/government_service.dart
 
-import 'package:dio/dio.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../models/government_model.dart';
-import '../utils/mon_pays_constants.dart';
 
 class GovernmentService {
-  final Dio _dio;
+  final SupabaseClient _supabase;
 
-  GovernmentService(this._dio);
+  GovernmentService(this._supabase);
 
   Future<List<Government>> getAll() async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}/government');
-    return (response.data as List).map((e) => Government.fromJson(e)).toList();
+    final response = await _supabase
+        .from('governments')
+        .select('*');
+    return (response as List).map((e) => Government.fromJson(e)).toList();
   }
 
   Future<Government> getById(String id) async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}/government/$id');
-    return Government.fromJson(response.data);
+    final response = await _supabase
+        .from('governments')
+        .select('*')
+        .eq('id', id)
+        .single();
+    return Government.fromJson(response);
   }
 
   Future<Government> create(Government gov) async {
-    final response = await _dio.post(
-      '${MonPaysConstants.baseUrl}/government',
-      data: gov.toJson(),
-    );
-    return Government.fromJson(response.data);
+    final response = await _supabase
+        .from('governments')
+        .insert(gov.toJson())
+        .select()
+        .single();
+    return Government.fromJson(response);
   }
 
   Future<Government> update(Government gov) async {
-    final response = await _dio.put(
-      '${MonPaysConstants.baseUrl}/government/${gov.id}',
-      data: gov.toJson(),
-    );
-    return Government.fromJson(response.data);
+    final response = await _supabase
+        .from('governments')
+        .update(gov.toJson())
+        .eq('id', gov.id)
+        .select()
+        .single();
+    return Government.fromJson(response);
   }
 
   Future<void> delete(String id) async {
-    await _dio.delete('${MonPaysConstants.baseUrl}/government/$id');
+    await _supabase
+        .from('governments')
+        .delete()
+        .eq('id', id);
   }
 }
