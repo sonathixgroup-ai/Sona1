c9a1281b5ff9a9e4e859afb759798930127d852e
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-10 14:46:32 +0300

    Update history_service.dart

--- lib/presentation/mon_pays/services/history_service.dart
+++ lib/presentation/mon_pays/services/history_service.dart
@@ -1,41 +1,52 @@
 // lib/presentation/mon_pays/services/history_service.dart
 
-import 'package:dio/dio.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../models/history_model.dart';
-import '../utils/mon_pays_constants.dart';
 
 class HistoryService {
-  final Dio _dio;
+  final SupabaseClient _supabase;
 
-  HistoryService(this._dio);
+  HistoryService(this._supabase);
 
   Future<List<HistoricalFigure>> getAll() async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.historyEndpoint}');
-    return (response.data as List).map((e) => HistoricalFigure.fromJson(e)).toList();
+    final response = await _supabase
+        .from('historical_figures')
+        .select('*');
+    return (response as List).map((e) => HistoricalFigure.fromJson(e)).toList();
   }
 
   Future<HistoricalFigure> getById(String id) async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.historyEndpoint}/$id');
-    return HistoricalFigure.fromJson(response.data);
+    final response = await _supabase
+        .from('historical_figures')
+        .select('*')
+        .eq('id', id)
+        .single();
+    return HistoricalFigure.fromJson(response);
   }
 
   Future<HistoricalFigure> create(HistoricalFigure figure) async {
-    final response = await _dio.post(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.historyEndpoint}',
-      data: figure.toJson(),
-    );
-    return HistoricalFigure.fromJson(response.data);
+    final response = await _supabase
+        .from('historical_figures')
+        .insert(figure.toJson())
+        .select()
+        .single();
+    return HistoricalFigure.fromJson(response);
   }
 
   Future<HistoricalFigure> update(HistoricalFigure figure) async {
-    final response = await _dio.put(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.historyEndpoint}/${figure.id}',
-      data: figure.toJson(),
-    );
-    return HistoricalFigure.fromJson(response.data);
+    final response = await _supabase
+        .from('historical_figures')
+        .update(figure.toJson())
+        .eq('id', figure.id)
+        .select()
+        .single();
+    return HistoricalFigure.fromJson(response);
   }
 
   Future<void> delete(String id) async {
-    await _dio.delete('${MonPaysConstants.baseUrl}${MonPaysConstants.historyEndpoint}/$id');
+    await _supabase
+        .from('historical_figures')
+        .delete()
+        .eq('id', id);
   }
 }
