946e5a25ebde8c0b030eaa651f1de9002b105f15
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-10 14:50:27 +0300

    Update citizens_service.dart

--- lib/presentation/mon_pays/services/citizens_service.dart
+++ lib/presentation/mon_pays/services/citizens_service.dart
@@ -1,41 +1,52 @@
 // lib/presentation/mon_pays/services/citizens_service.dart
 
-import 'package:dio/dio.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../models/citizen_model.dart';
-import '../utils/mon_pays_constants.dart';
 
 class CitizensService {
-  final Dio _dio;
+  final SupabaseClient _supabase;
 
-  CitizensService(this._dio);
+  CitizensService(this._supabase);
 
   Future<List<ExemplaryCitizen>> getAll() async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.citizensEndpoint}');
-    return (response.data as List).map((e) => ExemplaryCitizen.fromJson(e)).toList();
+    final response = await _supabase
+        .from('exemplary_citizens')
+        .select('*');
+    return (response as List).map((e) => ExemplaryCitizen.fromJson(e)).toList();
   }
 
   Future<ExemplaryCitizen> getById(String id) async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.citizensEndpoint}/$id');
-    return ExemplaryCitizen.fromJson(response.data);
+    final response = await _supabase
+        .from('exemplary_citizens')
+        .select('*')
+        .eq('id', id)
+        .single();
+    return ExemplaryCitizen.fromJson(response);
   }
 
   Future<ExemplaryCitizen> create(ExemplaryCitizen citizen) async {
-    final response = await _dio.post(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.citizensEndpoint}',
-      data: citizen.toJson(),
-    );
-    return ExemplaryCitizen.fromJson(response.data);
+    final response = await _supabase
+        .from('exemplary_citizens')
+        .insert(citizen.toJson())
+        .select()
+        .single();
+    return ExemplaryCitizen.fromJson(response);
   }
 
   Future<ExemplaryCitizen> update(ExemplaryCitizen citizen) async {
-    final response = await _dio.put(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.citizensEndpoint}/${citizen.id}',
-      data: citizen.toJson(),
-    );
-    return ExemplaryCitizen.fromJson(response.data);
+    final response = await _supabase
+        .from('exemplary_citizens')
+        .update(citizen.toJson())
+        .eq('id', citizen.id)
+        .select()
+        .single();
+    return ExemplaryCitizen.fromJson(response);
   }
 
   Future<void> delete(String id) async {
-    await _dio.delete('${MonPaysConstants.baseUrl}${MonPaysConstants.citizensEndpoint}/$id');
+    await _supabase
+        .from('exemplary_citizens')
+        .delete()
+        .eq('id', id);
   }
 }
