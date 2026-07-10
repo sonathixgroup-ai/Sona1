d8848a43cf2849958ed090b410b9cf7fd6690a3e
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-10 14:49:00 +0300

    Update documentaries_service.dart

--- lib/presentation/mon_pays/services/documentaries_service.dart
+++ lib/presentation/mon_pays/services/documentaries_service.dart
@@ -1,41 +1,52 @@
 // lib/presentation/mon_pays/services/documentaries_service.dart
 
-import 'package:dio/dio.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../models/documentary_model.dart';
-import '../utils/mon_pays_constants.dart';
 
 class DocumentariesService {
-  final Dio _dio;
+  final SupabaseClient _supabase;
 
-  DocumentariesService(this._dio);
+  DocumentariesService(this._supabase);
 
   Future<List<Documentary>> getAll() async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.documentariesEndpoint}');
-    return (response.data as List).map((e) => Documentary.fromJson(e)).toList();
+    final response = await _supabase
+        .from('documentaries')
+        .select('*');
+    return (response as List).map((e) => Documentary.fromJson(e)).toList();
   }
 
   Future<Documentary> getById(String id) async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.documentariesEndpoint}/$id');
-    return Documentary.fromJson(response.data);
+    final response = await _supabase
+        .from('documentaries')
+        .select('*')
+        .eq('id', id)
+        .single();
+    return Documentary.fromJson(response);
   }
 
-  Future<Documentary> create(Documentary documentary) async {
-    final response = await _dio.post(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.documentariesEndpoint}',
-      data: documentary.toJson(),
-    );
-    return Documentary.fromJson(response.data);
+  Future<Documentary> create(Documentary doc) async {
+    final response = await _supabase
+        .from('documentaries')
+        .insert(doc.toJson())
+        .select()
+        .single();
+    return Documentary.fromJson(response);
   }
 
-  Future<Documentary> update(Documentary documentary) async {
-    final response = await _dio.put(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.documentariesEndpoint}/${documentary.id}',
-      data: documentary.toJson(),
-    );
-    return Documentary.fromJson(response.data);
+  Future<Documentary> update(Documentary doc) async {
+    final response = await _supabase
+        .from('documentaries')
+        .update(doc.toJson())
+        .eq('id', doc.id)
+        .select()
+        .single();
+    return Documentary.fromJson(response);
   }
 
   Future<void> delete(String id) async {
-    await _dio.delete('${MonPaysConstants.baseUrl}${MonPaysConstants.documentariesEndpoint}/$id');
+    await _supabase
+        .from('documentaries')
+        .delete()
+        .eq('id', id);
   }
 }
