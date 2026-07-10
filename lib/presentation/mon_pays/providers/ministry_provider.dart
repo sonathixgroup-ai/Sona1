5da672a375046debd6bf22fa2b8580b3377cc733
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-10 14:25:59 +0300

    Update ministry_provider.dart

--- lib/presentation/mon_pays/providers/ministry_provider.dart
+++ lib/presentation/mon_pays/providers/ministry_provider.dart
@@ -3,16 +3,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/ministry_model.dart';
 import '../repositories/ministry_repository.dart';
-import '../services/ministry_service.dart';
 import 'mon_pays_provider.dart';
-
-final ministryServiceProvider = Provider<MinistryService>(
-  (ref) => MinistryService(ref.watch(dioProvider)),
-);
-
-final ministryRepositoryProvider = Provider<MinistryRepository>(
-  (ref) => MinistryRepository(ref.watch(ministryServiceProvider)),
-);
 
 final ministriesProvider = FutureProvider<List<Ministry>>((ref) async {
   final repo = ref.watch(ministryRepositoryProvider);
