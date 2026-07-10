28345b659d29d1a6e920e30833cc56d556ffc60d
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-10 14:45:30 +0300

    Update ministry_service.dart

--- lib/presentation/mon_pays/services/ministry_service.dart
+++ lib/presentation/mon_pays/services/ministry_service.dart
@@ -1,41 +1,52 @@
 // lib/presentation/mon_pays/services/ministry_service.dart
 
-import 'package:dio/dio.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../models/ministry_model.dart';
-import '../utils/mon_pays_constants.dart';
 
 class MinistryService {
-  final Dio _dio;
+  final SupabaseClient _supabase;
 
-  MinistryService(this._dio);
+  MinistryService(this._supabase);
 
   Future<List<Ministry>> getAll() async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.ministriesEndpoint}');
-    return (response.data as List).map((e) => Ministry.fromJson(e)).toList();
+    final response = await _supabase
+        .from('ministries')
+        .select('*');
+    return (response as List).map((e) => Ministry.fromJson(e)).toList();
   }
 
   Future<Ministry> getById(String id) async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.ministriesEndpoint}/$id');
-    return Ministry.fromJson(response.data);
+    final response = await _supabase
+        .from('ministries')
+        .select('*')
+        .eq('id', id)
+        .single();
+    return Ministry.fromJson(response);
   }
 
   Future<Ministry> create(Ministry ministry) async {
-    final response = await _dio.post(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.ministriesEndpoint}',
-      data: ministry.toJson(),
-    );
-    return Ministry.fromJson(response.data);
+    final response = await _supabase
+        .from('ministries')
+        .insert(ministry.toJson())
+        .select()
+        .single();
+    return Ministry.fromJson(response);
   }
 
   Future<Ministry> update(Ministry ministry) async {
-    final response = await _dio.put(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.ministriesEndpoint}/${ministry.id}',
-      data: ministry.toJson(),
-    );
-    return Ministry.fromJson(response.data);
+    final response = await _supabase
+        .from('ministries')
+        .update(ministry.toJson())
+        .eq('id', ministry.id)
+        .select()
+        .single();
+    return Ministry.fromJson(response);
   }
 
   Future<void> delete(String id) async {
-    await _dio.delete('${MonPaysConstants.baseUrl}${MonPaysConstants.ministriesEndpoint}/$id');
+    await _supabase
+        .from('ministries')
+        .delete()
+        .eq('id', id);
   }
 }
