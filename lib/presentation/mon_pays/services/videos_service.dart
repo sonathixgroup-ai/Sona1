01b0a00bc1afbd21e3e0fe50286effbe97b50ce0
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-10 14:48:34 +0300

    Update videos_service.dart

--- lib/presentation/mon_pays/services/videos_service.dart
+++ lib/presentation/mon_pays/services/videos_service.dart
@@ -1,41 +1,52 @@
 // lib/presentation/mon_pays/services/videos_service.dart
 
-import 'package:dio/dio.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../models/video_model.dart';
-import '../utils/mon_pays_constants.dart';
 
 class VideosService {
-  final Dio _dio;
+  final SupabaseClient _supabase;
 
-  VideosService(this._dio);
+  VideosService(this._supabase);
 
   Future<List<Video>> getAll() async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.videosEndpoint}');
-    return (response.data as List).map((e) => Video.fromJson(e)).toList();
+    final response = await _supabase
+        .from('videos')
+        .select('*');
+    return (response as List).map((e) => Video.fromJson(e)).toList();
   }
 
   Future<Video> getById(String id) async {
-    final response = await _dio.get('${MonPaysConstants.baseUrl}${MonPaysConstants.videosEndpoint}/$id');
-    return Video.fromJson(response.data);
+    final response = await _supabase
+        .from('videos')
+        .select('*')
+        .eq('id', id)
+        .single();
+    return Video.fromJson(response);
   }
 
   Future<Video> create(Video video) async {
-    final response = await _dio.post(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.videosEndpoint}',
-      data: video.toJson(),
-    );
-    return Video.fromJson(response.data);
+    final response = await _supabase
+        .from('videos')
+        .insert(video.toJson())
+        .select()
+        .single();
+    return Video.fromJson(response);
   }
 
   Future<Video> update(Video video) async {
-    final response = await _dio.put(
-      '${MonPaysConstants.baseUrl}${MonPaysConstants.videosEndpoint}/${video.id}',
-      data: video.toJson(),
-    );
-    return Video.fromJson(response.data);
+    final response = await _supabase
+        .from('videos')
+        .update(video.toJson())
+        .eq('id', video.id)
+        .select()
+        .single();
+    return Video.fromJson(response);
   }
 
   Future<void> delete(String id) async {
-    await _dio.delete('${MonPaysConstants.baseUrl}${MonPaysConstants.videosEndpoint}/$id');
+    await _supabase
+        .from('videos')
+        .delete()
+        .eq('id', id);
   }
 }
