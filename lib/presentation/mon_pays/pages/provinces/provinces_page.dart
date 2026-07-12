b34ed93889e23e6e05c51fb261cd079eecfa7ec7
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-12 17:49:11 +0300

    Update provinces_page.dart

--- lib/presentation/mon_pays/pages/provinces/provinces_page.dart
+++ lib/presentation/mon_pays/pages/provinces/provinces_page.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import '../../models/province.dart';
 import '../../providers/provinces_provider.dart';
 import '../../cards/province_card.dart';
 
