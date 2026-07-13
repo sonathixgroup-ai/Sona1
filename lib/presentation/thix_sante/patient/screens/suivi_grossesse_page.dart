2df027d5a900ccaeae8337232dd9d83e9474e5f4
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-13 10:03:47 +0300

    Update suivi_grossesse_page.dart

--- lib/presentation/thix_sante/patient/screens/suivi_grossesse_page.dart
+++ lib/presentation/thix_sante/patient/screens/suivi_grossesse_page.dart
@@ -9,7 +9,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
 import '../../core/thix_id_validator.dart';
+import '../../core/thix_sante_colors.dart';
 import '../../patient/models/health_record_model.dart';
 
 final grossesseRecordsProvider = FutureProvider<List<HealthRecordModel>>((ref) async {
