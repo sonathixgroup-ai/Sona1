8a16afa66bff6052ec5a16bcd751111fc474df1f
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-13 09:58:33 +0300

    Update bien_etre_mental_page.dart

--- lib/presentation/thix_sante/patient/screens/bien_etre_mental_page.dart
+++ lib/presentation/thix_sante/patient/screens/bien_etre_mental_page.dart
@@ -9,7 +9,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
+import '../../core/thix_id_validator.dart';
 import '../../core/thix_sante_colors.dart';
 
 final moodProvider = FutureProvider<List<Map<String,dynamic>>>((ref) async {
