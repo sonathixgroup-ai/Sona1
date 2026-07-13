aa28fd693111aca6a1daaa7bbc20ab542993ee83
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-13 10:12:54 +0300

    Update gestion_stress_page.dart

--- lib/presentation/thix_sante/patient/screens/gestion_stress_page.dart
+++ lib/presentation/thix_sante/patient/screens/gestion_stress_page.dart
@@ -11,6 +11,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../core/thix_id_validator.dart';
+import '../../core/thix_sante_colors.dart';
 
 final stressLogsProvider = FutureProvider<List<Map<String, dynamic>>>((ref) async {
   final db = Supabase.instance.client;
