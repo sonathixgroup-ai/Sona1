93a3d3d75d79b133fb58b973793b2a8b6f82a837
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-13 05:42:23 +0300

    Delete lib/features/thix_sante directory

--- lib/features/thix_sante/domain/repositories/consultation_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:thix_id/features/thix_sante/domain/models/consultation_model.dart';
-
-abstract class ConsultationRepository {
-  Stream<List<ConsultationModel>> watchConsultations({required String patientId});
-  Future<List<ConsultationModel>> fetchConsultations({required String patientId, int limit = 50, int offset = 0});
-}
--- lib/features/thix_sante/domain/repositories/medication_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:thix_id/features/thix_sante/domain/models/medication_model.dart';
-
-abstract class MedicationRepository {
-  Stream<List<MedicationModel>> watchMedications({required String patientId, bool activeOnly = false});
-  Future<List<MedicationModel>> fetchMedications({required String patientId, bool activeOnly = false, int limit = 50, int offset = 0});
-}
