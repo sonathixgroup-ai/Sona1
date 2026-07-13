274cc2400b15a83e4d68ffe409304499cae5c505
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-13 10:34:12 +0300

    Update mon_medecin_traitant_page.dart

--- lib/presentation/thix_sante/patient/screens/mon_medecin_traitant_page.dart
+++ lib/presentation/thix_sante/patient/screens/mon_medecin_traitant_page.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../core/thix_id_validator.dart';
+import '../../core/thix_sante_colors.dart';
 import '../models/doctor_profile_model.dart';
 import '../providers/patient_dashboard_provider.dart';
 
