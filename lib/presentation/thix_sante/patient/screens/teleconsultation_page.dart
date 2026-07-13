8eb3e4c661b29ced6ca85a47664a033196cf01fd
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-13 11:13:47 +0300

    Update teleconsultation_page.dart

--- lib/presentation/thix_sante/patient/screens/teleconsultation_page.dart
+++ lib/presentation/thix_sante/patient/screens/teleconsultation_page.dart
@@ -1,7 +1,7 @@
 // lib/presentation/thix_sante/patient/screens/teleconsultation_page.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../core/thix_sante_theme.dart';
+import '../../core/thix_sante_colors.dart';
 
 class TeleconsultationPage extends ConsumerStatefulWidget {
   const TeleconsultationPage({super.key});
