411a5da1feef43d2d4f3cc0251f358aeecbb99d9
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-14 09:22:59 +0300

    Update admin_article_form_page.dart

--- lib/presentation/admin/admin_article_form_page.dart
+++ lib/presentation/admin/admin_article_form_page.dart
@@ -7,9 +7,10 @@ import 'package:video_player/video_player.dart';
 import '../../providers/news_provider.dart';
 import '../../models/news_article.dart';
 
-const _kGold = Color(0xFFFFB800);
+coconst _kGold = Color(0xFFFFB800);
 const _kDark = Color(0xFF101840);
 const _kBorder = Color(0xFFECEEF4);
+const _kWhite = Colors.white;
 
 class AdminArticleFormPage extends StatefulWidget {
   final String? articleId;
