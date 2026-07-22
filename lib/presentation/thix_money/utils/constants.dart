5f1b81d85c8de1bb172428a84c40b45de8c55cfb
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-22 19:33:58 +0300

    Create constants.dart

---
+++ lib/presentation/thix_money/utils/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ThixConstants {
+  static const primary = Color(0xFF1A3FFF);
+  static const darkBlue = Color(0xFF0A2A8A);
+  static const bg = Color(0xFFF6F8FF);
+  
+  static const supportedDevises = ['CDF', 'USD'];
+  static const minCdf = 1000;
+  static const maxCdf = 10000000;
+  static const minUsd = 1;
+  static const maxUsd = 5000;
+
+  static const pageSize = 20; // Pagination pour millions d'users
+  static const wonyaFunctionInit = 'wonya-init';
+  static const wonyaFunctionCallback = 'wonya-callback';
+}
