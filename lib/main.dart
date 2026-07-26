3bc260b56618769a34ca3aeb42e32d1e09d73260
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-26 14:17:57 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+// lib/main.dart
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:provider/provider.dart' as app_provider;
+
 import 'package:thix_id/auth/auth_controller.dart';
 import 'package:thix_id/auth/supabase_auth_manager.dart';
 import 'package:thix_id/l10n/app_localizations.dart';
@@ -39,10 +41,13 @@ Future<void> main() async {
       debugPrint('FlutterError: ${d.exceptionAsString()}');
       if (kDebugMode) FlutterError.presentError(d);
     };
-    PlatformDispatcher.instance.onError = (e,s){ debugPrint('$e'); return true; };
+    PlatformDispatcher.instance.onError = (e, s) { 
+      debugPrint('$e'); 
+      return true; 
+    };
 
     runApp(const ProviderScope(child: MyApp()));
-  }, (e,s)=> debugPrint('Zone: $e'));
+  }, (e, s) => debugPrint('Zone: $e'));
 }
 
 class MyApp extends ConsumerStatefulWidget {
@@ -67,11 +72,12 @@ class _MyAppState extends ConsumerState<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    // FIX CRITIQUE : on fournit les 2 types de Provider
+    // Les "Core Providers" indispensables pour que l'app s'ouvre et se connecte
     return app_provider.MultiProvider(
       providers: [
         app_provider.ChangeNotifierProvider<AuthController>.value(value: _auth),
         app_provider.ChangeNotifierProvider<LocaleController>.value(value: _locale),
+        app_provider.Provider<ProfileService>(create: (_) => ProfileService()),
       ],
       child: MaterialApp.router(
         title: 'THIX ID CENTRAL',
