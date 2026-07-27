5216af89f37c387738120211aa29d6d95e19d9ba
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-27 16:12:30 +0300

    Create market_colors.dart

---
+++ lib/presentation/thix_market/core/market_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MarketColors {
+  static const redDark = Color(0xFF5C0E12);
+  static const red = Color(0xFFD81E2C);
+  static const redSoft = Color(0xFFE63946);
+  static const gold = Color(0xFFF0A93B);
+  static const creamBg = Color(0xFFFCEFDA);
+  static const lightBg = Color(0xFFF7F7FA);
+  static const white = Color(0xFFFFFFFF);
+  static const darkText = Color(0xFF1A1A1A);
+  static const mutedText = Color(0xFF8A8A8F);
+  static const cardBorder = Color(0xFFF0F0F0);
+}
