eba33662ca8f1ab571d11945c9682afc1bf48fcc
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-27 03:26:54 +0300

    Update network_service_provider.dart

--- lib/features/network/data/network_service_provider.dart
+++ lib/features/network/data/network_service_provider.dart
@@ -1,13 +1,6 @@
-import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:thix_id/services/network_service.dart';
-part 'network_service_provider.g.dart';
 
-@riverpod
-SupabaseClient supabaseClient(SupabaseClientRef ref) => Supabase.instance.client;
-
-@riverpod
-NetworkService networkService(NetworkServiceRef ref) {
-  final client = ref.watch(supabaseClientProvider);
-  return NetworkService(client);
-}
+final supabaseClientProvider = Provider<SupabaseClient>((ref) => Supabase.instance.client);
+final networkServiceProvider = Provider<NetworkService>((ref) => NetworkService(ref.read(supabaseClientProvider)));
