00a8e05dcc11c139148cf4a2c6c3eeb2567eefe8
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-28 07:22:38 +0300

    Update admin_events_page.dart

--- lib/presentation/admin/pages/admin_events_page.dart
+++ lib/presentation/admin/pages/admin_events_page.dart
@@ -1,23 +1,15 @@
 // lib/presentation/admin/pages/admin_events_page.dart
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'package:provider/provider.dart' as legacy_provider;
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'admin_events_dashboard.dart';
 
-import '../../../providers/event_provider.dart';
-import '../../../services/event_service.dart';
-import 'admin_events_dashboard.dart';  // Même dossier
-
-class AdminEventsPage extends StatelessWidget {
+class AdminEventsPage extends ConsumerWidget {
   final String role;
   
   const AdminEventsPage({super.key, required this.role});
 
   @override
-  Widget build(BuildContext context) {
-    return legacy_provider.ChangeNotifierProvider<EventProvider>(
-      create: (_) => EventProvider(EventService(Supabase.instance.client)),
-      child: const AdminEventsDashboard(),
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const AdminEventsDashboard();
   }
 }
