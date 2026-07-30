61afd50507e49d9c110c07232c1281cd31184146
sonathixgroup-ai <sonathixgroup@gmail.com>
2026-07-30 18:16:09 +0300

    Update formation_lesson_player.dart

--- lib/presentation/education/widgets/formation_detail/formation_lesson_player.dart
+++ lib/presentation/education/widgets/formation_detail/formation_lesson_player.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import '../../../models/lesson.dart';
+import 'package:thix_id/presentation/education/models/lesson.dart';
 import 'formation_video_player.dart';
 import 'formation_evaluation_widget.dart';
 
